3cc0557b051ccbe2417a00469bbe77889b6c0c41
Yaroslav Litavrin <yarillo@ngs.ru>
2022-12-11 18:55:59 +0600

    444

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import re
-import pymorphy2
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-from wordcloud import WordCloud
+#import re
+#import pymorphy2
+#import nltk
+#from nltk.corpus import stopwords
+#nltk.download('stopwords')
+#from wordcloud import WordCloud
 
 
 st.set_page_config(layout="wide")   # Полнооконное представление приложения
@@ -98,75 +98,6 @@ def users_top(df):
             st.pyplot(f)
 
 
-def preprocess(lst):
-    '''Функция принимает на вход список сообщений, предобрабатывает его,
-       и возвращет в виде мешка слов'''
-    # Приводим к нижнему регистру
-    bag = ' '.join(lst).lower()
-    # Убираем цифры
-    numbers = re.compile(r'\d+')
-    bag = re.sub(numbers, '', bag)
-    # Убираем пунктуацию
-    punct = re.compile(r'[^\w\s]')
-    bag = re.sub(punct, '', bag)
-    # Удаляем ссылки
-    url = re.compile(r'((?<=[^a-zA-Z0-9])(?:https?\:\/\/|[a-zA-Z0-9]{1,}\.{1}|\b)(?:\w{1,}\.{1}){1,5}(?:com|co|org|edu|gov|uk|net|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es|mil|iq|io|ac|ly|sm){1}(?:\/[a-zA-Z0-9]{1,})*)')
-    bag = re.sub(url, '', bag)
-    # Убираем спецсимволы:
-    symbols = re.compile(r'\n')
-    bag = re.sub(symbols, '', bag)
-    # Убираем лишние пробелы
-    #extra_spaces = re.compile(r'\s{2,}')
-    #bag = re.sub(extra_spaces, ' ', bag)
-    # Лемматизация и очистка от стопслов
-    morph = pymorphy2.MorphAnalyzer()
-    stop = stopwords.words('russian')
-    stop.extend(['это', 'весь', 'всё', 'наш', 'ваш', 'который', 'почему'])
-    bag = [morph.normal_forms(word)[0] for word in bag.split() if word not in stop]
-    return ' '.join(bag)
-
-
-def show_wordcloud(bag):
-    wordcloud = WordCloud(
-            width = 800, 
-            height = 800,
-            background_color ='white',
-            min_font_size = 10
-        ).generate(bag)
-
-    ff = plt.figure(figsize=(5, 5))
-    plt.imshow(wordcloud)
-    plt.axis('off')
-    st.pyplot(ff) 
-
-
-def make_clouds(df):
-    '''
-    Все функции, в которых есть надпись cloud, работают над созданием облаков слов
-    Вот именно эта выводит их в отдельной разворачивающейся вкладке
-    Отдельно по всем негативным комментам, отдельно - по позитивным
-    '''
-    positive_list = []
-    negative_list = []
-    positive_texts = df.index[df.loc[:, 'sentiment'] == 'POSITIVE'].tolist()
-    for idx in positive_texts:
-        positive_list.append(df.loc[idx, 'text'])
-
-    negative_texts = df.index[df.loc[:, 'sentiment'] == 'NEGATIVE'].tolist()
-    for idx in negative_texts:
-        negative_list.append(df.loc[idx, 'text'])
-
-    positive_bag = preprocess(positive_list)
-    negative_bag = preprocess(negative_list)
-
-    with st.expander("Облака слов по положительным и отрицательным сообщениям"):
-        col3, col4 = st.columns(2)
-        with col3:
-            show_wordcloud(positive_bag)
-        with col4:
-            show_wordcloud(negative_bag)
-
-
 data_url = 'https://raw.githubusercontent.com/YaRoLit/ml_test/main/Dev/Data/chat_sentiment.csv'
 
 
@@ -191,27 +122,3 @@ if result:
     time_histplot(df)
     sentpie_and_chat(df)
     users_top(df)
-    make_clouds(df)
-
-    with st.expander("Выбор интересущего периода, пользователя и тональности"):
-        col1, col2, col3 = st.columns(3)
-        
-        with col1:    
-            date = st.date_input('Выберите дату для просмотра активности', 
-                                #value=datetime.date,
-                                #min_value=df.date[df.shape[0]],
-                                #max_value=df.date[0],
-                                help='По выбранной дате будет дополнительная статистика')
-
-        with col2:    
-            choice = st.selectbox(label='Выберите тональность', 
-                            options=('NEUTRAL', 'POSITIVE', 'NEGATIVE'),
-                            help='Выберите интересующую тональность для анализа соответствующих сообщений')
-
-        with col3:    
-            choice = st.selectbox(label='Выберите пользователя', 
-                            options=('1','2','3'),
-                            index=0,
-                            help='Выберите интересующего пользователя для анализа его сообщений')
-        
-        res = st.button('Проанализировать выбранные параметры', )
